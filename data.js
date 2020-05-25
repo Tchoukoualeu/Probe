2fb67273792c06cb46afe31744ea6903a01cb5e1
Tchoukoualeu <38705757+Tchoukoualeu@users.noreply.github.com>
2020-05-25 13:42:15 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,676 @@
+export const data = [
+  {
+    id: "cf638b09-1afd-4690-99df-d256deb27848",
+    first_name: "Ebonee",
+    last_name: "Thaim",
+    email: "ethaim0@joomla.org",
+    gender: "Female",
+    imgURL: "https://robohash.org/molestiasnihilquia.jpg?size=50x50&set=set1",
+    birthDate: "1998-07-11",
+    friends: [
+      {
+        gender: "Female",
+        first_name: "Ekaterina",
+        last_name: "Le Fevre",
+        shisrtSize: "L"
+      },
+      {
+        gender: "Female",
+        first_name: "Britney",
+        last_name: "Lindermann",
+        shisrtSize: "L"
+      },
+      {
+        gender: "Female",
+        first_name: "Rivi",
+        last_name: "Scholes",
+        shisrtSize: "XL"
+      }
+    ]
+  },
+  {
+    id: "d533e517-f8cb-431b-a764-c7313b9d0ada",
+    first_name: "Haily",
+    last_name: "Fairy",
+    email: "hfairy1@google.de",
+    gender: "Female",
+    imgURL: "https://robohash.org/autsimiliqueea.jpg?size=50x50&set=set1",
+    birthDate: "1996-07-05",
+    friends: [
+      {
+        gender: "Female",
+        first_name: "Taffy",
+        last_name: "Guidera",
+        shisrtSize: "M"
+      },
+      {
+        gender: "Female",
+        first_name: "Helen-elizabeth",
+        last_name: "Wones",
+        shisrtSize: "3XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Web",
+        last_name: "Neary",
+        shisrtSize: "XL"
+      }
+    ]
+  },
+  {
+    id: "3beb0bbc-b213-4516-af67-1d991c878bea",
+    first_name: "Alfi",
+    last_name: "Kembery",
+    email: "akembery2@plala.or.jp",
+    gender: "Female",
+    imgURL: "https://robohash.org/eumauteveniet.jpg?size=50x50&set=set1",
+    birthDate: "1996-02-22",
+    friends: [
+      {
+        gender: "Female",
+        first_name: "Cayla",
+        last_name: "Corness",
+        shisrtSize: "S"
+      },
+      {
+        gender: "Male",
+        first_name: "Erskine",
+        last_name: "Sanham",
+        shisrtSize: "XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Doy",
+        last_name: "Hare",
+        shisrtSize: "3XL"
+      }
+    ]
+  },
+  {
+    id: "a6d9ccd3-1cfd-492d-a181-a21453d80e57",
+    first_name: "Odell",
+    last_name: "Abley",
+    email: "oabley3@wisc.edu",
+    gender: "Male",
+    imgURL: "https://robohash.org/quietcommodi.jpg?size=50x50&set=set1",
+    birthDate: "1999-07-03",
+    friends: [
+      {
+        gender: "Male",
+        first_name: "Patrizius",
+        last_name: "Loveland",
+        shisrtSize: "2XL"
+      },
+      {
+        gender: "Female",
+        first_name: "Shari",
+        last_name: "Nattriss",
+        shisrtSize: "2XL"
+      },
+      {
+        gender: "Female",
+        first_name: "Christye",
+        last_name: "Styan",
+        shisrtSize: "3XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Matthew",
+        last_name: "Joincey",
+        shisrtSize: "2XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Fitz",
+        last_name: "Belhomme",
+        shisrtSize: "L"
+      }
+    ]
+  },
+  {
+    id: "6ea23263-1fdf-4500-87f6-9ca6be6f4a27",
+    first_name: "Friedrich",
+    last_name: "Baylie",
+    email: "fbaylie4@techcrunch.com",
+    gender: "Male",
+    imgURL:
+      "https://robohash.org/nonmaioresconsequatur.jpg?size=50x50&set=set1",
+    birthDate: "1997-03-17",
+    friends: [
+      {
+        gender: "Male",
+        first_name: "Cello",
+        last_name: "Van T'Hoog",
+        shisrtSize: "XS"
+      },
+      {
+        gender: "Male",
+        first_name: "Giulio",
+        last_name: "Huton",
+        shisrtSize: "XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Neale",
+        last_name: "Gildroy",
+        shisrtSize: "3XL"
+      },
+      {
+        gender: "Female",
+        first_name: "Juliane",
+        last_name: "Crudge",
+        shisrtSize: "3XL"
+      }
+    ]
+  },
+  {
+    id: "590cbd0d-e98e-42e6-afcf-09c4d082f287",
+    first_name: "Kynthia",
+    last_name: "Poveleye",
+    email: "kpoveleye5@ezinearticles.com",
+    gender: "Female",
+    imgURL: "https://robohash.org/nihildoloremquenam.jpg?size=50x50&set=set1",
+    birthDate: "1997-11-24",
+    friends: [
+      {
+        gender: "Female",
+        first_name: "Kira",
+        last_name: "Schapiro",
+        shisrtSize: "XL"
+      },
+      {
+        gender: "Female",
+        first_name: "Hatti",
+        last_name: "Drioli",
+        shisrtSize: "2XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Silvio",
+        last_name: "Jordine",
+        shisrtSize: "2XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Chaddy",
+        last_name: "Haddington",
+        shisrtSize: "XS"
+      }
+    ]
+  },
+  {
+    id: "097fa6d4-a7e6-4c62-aaca-90801d0a791d",
+    first_name: "Hersh",
+    last_name: "Carass",
+    email: "hcarass6@mail.ru",
+    gender: "Male",
+    imgURL: "https://robohash.org/asperioreseteos.jpg?size=50x50&set=set1",
+    birthDate: "2000-05-02",
+    friends: [
+      {
+        gender: "Female",
+        first_name: "Corabel",
+        last_name: "Horche",
+        shisrtSize: "XS"
+      },
+      {
+        gender: "Male",
+        first_name: "Ernst",
+        last_name: "Rawcliffe",
+        shisrtSize: "XL"
+      },
+      {
+        gender: "Female",
+        first_name: "Mia",
+        last_name: "Manser",
+        shisrtSize: "XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Briggs",
+        last_name: "Jozwicki",
+        shisrtSize: "XS"
+      }
+    ]
+  },
+  {
+    id: "8b9efdb2-ceb8-48c0-b77c-08b4c3e6b2e5",
+    first_name: "Daffie",
+    last_name: "Saunter",
+    email: "dsaunter7@answers.com",
+    gender: "Female",
+    imgURL: "https://robohash.org/nullamaioresat.jpg?size=50x50&set=set1",
+    birthDate: "1996-05-12",
+    friends: [
+      {
+        gender: "Male",
+        first_name: "Burl",
+        last_name: "Rhoades",
+        shisrtSize: "XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Penn",
+        last_name: "Gresswood",
+        shisrtSize: "S"
+      },
+      {
+        gender: "Female",
+        first_name: "Candie",
+        last_name: "Martinez",
+        shisrtSize: "XL"
+      }
+    ]
+  },
+  {
+    id: "e742591c-681f-443e-8302-a6ac3b091ad0",
+    first_name: "Hasty",
+    last_name: "Nagle",
+    email: "hnagle8@house.gov",
+    gender: "Male",
+    imgURL:
+      "https://robohash.org/etlaudantiumaspernatur.jpg?size=50x50&set=set1",
+    birthDate: "1999-09-25",
+    friends: [
+      {
+        gender: "Female",
+        first_name: "Allina",
+        last_name: "Giacomello",
+        shisrtSize: "XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Pascal",
+        last_name: "Lankham",
+        shisrtSize: "2XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Ken",
+        last_name: "Armistead",
+        shisrtSize: "2XL"
+      }
+    ]
+  },
+  {
+    id: "763abd25-7b20-4128-bc1a-04453c4acf3b",
+    first_name: "Rob",
+    last_name: "Hallwell",
+    email: "rhallwell9@loc.gov",
+    gender: "Male",
+    imgURL: "https://robohash.org/saepedoloremquequi.jpg?size=50x50&set=set1",
+    birthDate: "1996-11-29",
+    friends: [
+      {
+        gender: "Male",
+        first_name: "Myles",
+        last_name: "Wyche",
+        shisrtSize: "S"
+      },
+      {
+        gender: "Male",
+        first_name: "Torrin",
+        last_name: "Cooper",
+        shisrtSize: "L"
+      },
+      {
+        gender: "Female",
+        first_name: "Thalia",
+        last_name: "Saura",
+        shisrtSize: "S"
+      },
+      {
+        gender: "Female",
+        first_name: "Emeline",
+        last_name: "Crosoer",
+        shisrtSize: "XS"
+      },
+      {
+        gender: "Male",
+        first_name: "Dionisio",
+        last_name: "Fillis",
+        shisrtSize: "XL"
+      }
+    ]
+  },
+  {
+    id: "5963abc7-810d-43ac-8e5e-52fe6f9fa991",
+    first_name: "Kala",
+    last_name: "Poundsford",
+    email: "kpoundsforda@goo.ne.jp",
+    gender: "Female",
+    imgURL: "https://robohash.org/seddeseruntquod.jpg?size=50x50&set=set1",
+    birthDate: "1999-02-01",
+    friends: [
+      {
+        gender: "Male",
+        first_name: "Hermann",
+        last_name: "Bownes",
+        shisrtSize: "L"
+      },
+      {
+        gender: "Female",
+        first_name: "Nady",
+        last_name: "Lampel",
+        shisrtSize: "XS"
+      },
+      {
+        gender: "Female",
+        first_name: "Cindelyn",
+        last_name: "Goney",
+        shisrtSize: "2XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Cheston",
+        last_name: "Reville",
+        shisrtSize: "S"
+      }
+    ]
+  },
+  {
+    id: "d6b60e1b-a527-4ac6-8d8b-62b8f5e660e8",
+    first_name: "Odelinda",
+    last_name: "Slyme",
+    email: "oslymeb@ihg.com",
+    gender: "Female",
+    imgURL: "https://robohash.org/consequunturquised.jpg?size=50x50&set=set1",
+    birthDate: "1998-02-27",
+    friends: [
+      {
+        gender: "Female",
+        first_name: "Scarlett",
+        last_name: "Starkie",
+        shisrtSize: "3XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Case",
+        last_name: "Lowers",
+        shisrtSize: "M"
+      },
+      {
+        gender: "Male",
+        first_name: "Kerry",
+        last_name: "Rutley",
+        shisrtSize: "L"
+      },
+      {
+        gender: "Male",
+        first_name: "Terencio",
+        last_name: "Dack",
+        shisrtSize: "M"
+      }
+    ]
+  },
+  {
+    id: "243bd909-cebb-450e-8644-5cc130f0a5f1",
+    first_name: "Rufe",
+    last_name: "Polglase",
+    email: "rpolglasec@washington.edu",
+    gender: "Male",
+    imgURL: "https://robohash.org/perspiciatissedqui.jpg?size=50x50&set=set1",
+    birthDate: "1997-03-31",
+    friends: [
+      {
+        gender: "Male",
+        first_name: "Osborn",
+        last_name: "Roe",
+        shisrtSize: "L"
+      },
+      {
+        gender: "Male",
+        first_name: "Tomas",
+        last_name: "Dionisetti",
+        shisrtSize: "XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Robbert",
+        last_name: "Gagen",
+        shisrtSize: "L"
+      },
+      {
+        gender: "Male",
+        first_name: "Wheeler",
+        last_name: "Sire",
+        shisrtSize: "2XL"
+      },
+      {
+        gender: "Female",
+        first_name: "Emma",
+        last_name: "Tash",
+        shisrtSize: "S"
+      }
+    ]
+  },
+  {
+    id: "9f4621fa-af08-46f6-a85f-64d00a518fa2",
+    first_name: "Thadeus",
+    last_name: "Geany",
+    email: "tgeanyd@posterous.com",
+    gender: "Male",
+    imgURL:
+      "https://robohash.org/repellendusautofficia.jpg?size=50x50&set=set1",
+    birthDate: "1996-12-18",
+    friends: [
+      {
+        gender: "Male",
+        first_name: "Rory",
+        last_name: "Di Giacomo",
+        shisrtSize: "XS"
+      },
+      {
+        gender: "Female",
+        first_name: "Fifi",
+        last_name: "Gyppes",
+        shisrtSize: "3XL"
+      },
+      {
+        gender: "Female",
+        first_name: "Chrystel",
+        last_name: "Petran",
+        shisrtSize: "XL"
+      }
+    ]
+  },
+  {
+    id: "d07fc4f0-f9d1-4f72-9b8b-a1a9765b4d8f",
+    first_name: "Blinny",
+    last_name: "Whiteland",
+    email: "bwhitelande@hhs.gov",
+    gender: "Female",
+    imgURL: "https://robohash.org/quidemporrodicta.jpg?size=50x50&set=set1",
+    birthDate: "1998-01-31",
+    friends: [
+      {
+        gender: "Male",
+        first_name: "Jarrod",
+        last_name: "Egel",
+        shisrtSize: "L"
+      },
+      {
+        gender: "Female",
+        first_name: "Sharleen",
+        last_name: "Attwoul",
+        shisrtSize: "XS"
+      },
+      {
+        gender: "Female",
+        first_name: "Ondrea",
+        last_name: "Josovich",
+        shisrtSize: "L"
+      }
+    ]
+  },
+  {
+    id: "b94256b1-c501-4531-be14-76947fbadff4",
+    first_name: "Sloane",
+    last_name: "Burnard",
+    email: "sburnardf@infoseek.co.jp",
+    gender: "Male",
+    imgURL: "https://robohash.org/aliasetconsequatur.jpg?size=50x50&set=set1",
+    birthDate: "1999-07-28",
+    friends: [
+      {
+        gender: "Female",
+        first_name: "Chastity",
+        last_name: "Davidowsky",
+        shisrtSize: "XL"
+      },
+      {
+        gender: "Female",
+        first_name: "Nanete",
+        last_name: "Drinkale",
+        shisrtSize: "S"
+      },
+      {
+        gender: "Male",
+        first_name: "Kristofor",
+        last_name: "Dytham",
+        shisrtSize: "2XL"
+      }
+    ]
+  },
+  {
+    id: "ec8fc8da-eba2-4031-b648-a3aff14c4050",
+    first_name: "Daven",
+    last_name: "Pamplin",
+    email: "dpampling@cbsnews.com",
+    gender: "Male",
+    imgURL: "https://robohash.org/iurequibusdamet.jpg?size=50x50&set=set1",
+    birthDate: "1995-09-12",
+    friends: [
+      {
+        gender: "Male",
+        first_name: "Evan",
+        last_name: "Zamorano",
+        shisrtSize: "2XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Caspar",
+        last_name: "Piatkowski",
+        shisrtSize: "M"
+      },
+      {
+        gender: "Male",
+        first_name: "David",
+        last_name: "Delafoy",
+        shisrtSize: "XS"
+      }
+    ]
+  },
+  {
+    id: "c276afc6-829e-4afe-9ba5-c2805bd32a4e",
+    first_name: "Orville",
+    last_name: "Brownhill",
+    email: "obrownhillh@com.com",
+    gender: "Male",
+    imgURL: "https://robohash.org/liberoautemaperiam.jpg?size=50x50&set=set1",
+    birthDate: "1996-07-11",
+    friends: [
+      {
+        gender: "Female",
+        first_name: "Chandra",
+        last_name: "Maundrell",
+        shisrtSize: "XL"
+      },
+      {
+        gender: "Female",
+        first_name: "Nissa",
+        last_name: "Doore",
+        shisrtSize: "2XL"
+      },
+      {
+        gender: "Female",
+        first_name: "Tobey",
+        last_name: "Pinches",
+        shisrtSize: "3XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Uriel",
+        last_name: "Redsall",
+        shisrtSize: "XL"
+      }
+    ]
+  },
+  {
+    id: "4c5f3905-d656-498a-a608-64f05c2fd72f",
+    first_name: "Anastasia",
+    last_name: "Mcimmie",
+    email: "amcimmiei@sphinn.com",
+    gender: "Female",
+    imgURL: "https://robohash.org/aperiamdoloresnam.jpg?size=50x50&set=set1",
+    birthDate: "1996-09-22",
+    friends: [
+      {
+        gender: "Female",
+        first_name: "Athena",
+        last_name: "Gillcrist",
+        shisrtSize: "2XL"
+      },
+      {
+        gender: "Female",
+        first_name: "Maitilde",
+        last_name: "Tender",
+        shisrtSize: "2XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Mendie",
+        last_name: "Yezafovich",
+        shisrtSize: "M"
+      },
+      {
+        gender: "Female",
+        first_name: "Petunia",
+        last_name: "Old",
+        shisrtSize: "XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Gil",
+        last_name: "Whittington",
+        shisrtSize: "2XL"
+      }
+    ]
+  },
+  {
+    id: "93a6b24f-c1b9-4c89-8551-852defdd7782",
+    first_name: "Giraud",
+    last_name: "Dodds",
+    email: "gdoddsj@seattletimes.com",
+    gender: "Male",
+    imgURL:
+      "https://robohash.org/doloresconsequatursoluta.jpg?size=50x50&set=set1",
+    birthDate: "1995-12-23",
+    friends: [
+      {
+        gender: "Female",
+        first_name: "Brandice",
+        last_name: "Brandsma",
+        shisrtSize: "XL"
+      },
+      {
+        gender: "Female",
+        first_name: "Adriana",
+        last_name: "Barber",
+        shisrtSize: "3XL"
+      },
+      {
+        gender: "Male",
+        first_name: "Tammie",
+        last_name: "Heminsley",
+        shisrtSize: "3XL"
+      },
+      {
+        gender: "Female",
+        first_name: "Blancha",
+        last_name: "Say",
+        shisrtSize: "L"
+      }
+    ]
+  }
+];
